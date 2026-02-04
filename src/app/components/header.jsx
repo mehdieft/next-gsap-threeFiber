433d0b08e63539a118e04977b7428d785cfc2565
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2026-02-04 19:53:23 +0330

    manipulating next js

---
+++ src/app/components/header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <>
+        <h1>this is header</h1>
+        <div className="bg-red-400 h-10 w-full"></div>
+        </>
+    )
+}
---
+++ src/app/gsap/page.jsx
@@ -0,0 +1,9 @@
+"use client"
+export default function Gsap(){
+    console.log("fuck youu this is client")
+    return(
+        <>
+        <h1>hi im gsap bro</h1>
+        </>
+    )
+}
