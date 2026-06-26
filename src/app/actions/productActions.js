39126c9223cfa602e29f8a32cd9542823b7124d8
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2026-06-26 17:54:26 +0330

    ok

--- src/app/actions/productActions.js
+++ src/app/actions/productActions.js
@@ -1,10 +1,29 @@
 "use server"
+import { redirect } from 'next/navigation' // 👈 این رو اضافه کن!
+import prisma  from '@/db';
+
 import sqlite from 'better-sqlite3'
 import { revalidatePath } from 'next/cache';
-const db=sqlite('products.sqlite')
    export async function deleteProduct(productId) {
     console.log("this is id",productId)
        
-        db.prepare(`DELETE FROM products WHERE id=?`).run(productId);
+        // db.prepare(`DELETE FROM products WHERE id=?`).run(productId);
         revalidatePath('/products','page')
+    }
+
+
+   export  const handleSubmit=async(formData)=>{
+        "use server";
+        console.log("im submit",formData)
+        const newProduct={
+            name:formData.get('name'),
+            price:parseFloat(formData.get('price')),
+            image:formData.get('image')
+        }
+        console.log("this is result",newProduct)
+        prisma.products.create({
+            data:newProduct
+        })
+      
+       revalidatePath('/products','page')
     }
--- src/db/index.js
+++
@@ -1,2 +0,0 @@
-const {PrismaClient}=require('@prisma/client')
-export const db=new PrismaClient()
