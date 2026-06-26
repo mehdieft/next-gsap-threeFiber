69c0fe8aaa2c0a0a97b3b08a5bf4c19d8e11a377
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2026-06-26 21:45:54 +0330

    finally create works

--- src/app/actions/productActions.js
+++ src/app/actions/productActions.js
@@ -1,5 +1,5 @@
 "use server"
-import prisma from '../lib/prisma'
+import {prisma} from '../lib/prisma'
 import sqlite from 'better-sqlite3'
 import { revalidatePath } from 'next/cache';
 const db=sqlite('products.sqlite')
@@ -8,10 +8,11 @@ const db=sqlite('products.sqlite')
        
         db.prepare(`DELETE FROM products WHERE id=?`).run(productId);
         insertHandler()
+        
         revalidatePath('/products','page')
     }
     const insertHandler=async()=>{
-        prisma.products.create({
+       await prisma.products.create({
             data:{
                 name:"test",
                 price:100,
--- src/app/lib/prisma.js
+++ src/app/lib/prisma.js
@@ -1,9 +1,9 @@
-import { PrismaClient } from '@prisma/client'
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
-const globalForPrisma = globalThis
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-export default prisma
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaBetterSqlite3({ url: connectionString });
+const prisma = new PrismaClient({ adapter });
+export { prisma };
