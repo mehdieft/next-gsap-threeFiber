cb167303c7648024a0e8ad87f09dc73ff6ab4c86
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2026-07-03 19:11:50 +0330

    ok text animation works

--- src/app/gsap/components/AnimatedText.jsx
+++ src/app/gsap/components/AnimatedText.jsx
@@ -1,33 +1,96 @@
-import {useGSAP} from '@gsap/react'
-import gsap from 'gsap' 
-import {useRef} from 'react'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import splitText from 'gsap/SplitText';
-gsap.registerPlugin(splitText,ScrollTrigger)
-export default function AnimatedText({text,delay,duration,scrub}){
-    const containerRef=useRef(null)
-    const textRef=useRef(null)
-    const splitRef=useRef(null)
-    useGSAP(()=>
-        {
-            const el=containerRef.current
-            if(!el)return
-             splitRef.current=splitText.create(textRef.current,{
-                type:"lines,words,chars",
-                autoSplit:true,
-                charsClass:'text-animation-char',
-                linesClass:'text-animation-line',
-                wordsClass:'text-animation-word'
-             })
-             const {line,chars}=splitRef.current
-             
-        },
-    {scope:containerRef})
-    return(
-        <>
-        <section ref={containerRef}>
-            <h1 ref={textRef} ></h1>
-        </section>
-        </>
-    )
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import splitText from "gsap/SplitText";
+gsap.registerPlugin(splitText, ScrollTrigger);
+export default function AnimatedText({
+  text="hello there friend hope you enjoy ",
+  delay = 0,
+  duration = 2,
+  scrub = false,
+}) {
+  const containerRef = useRef(null);
+  const textRef = useRef(null);
+  const splitRef = useRef(null);
+  useGSAP(
+    () => {
+      const el = containerRef.current;
+      if (!el) return;
+      splitRef.current = splitText.create(textRef.current, {
+        type: "lines,words,chars",
+        autoSplit: true,
+        charsClass: "text-animation-char",
+        linesClass: "text-animation-line",
+        wordsClass: "text-animation-word",
+      });
+      const { lines, chars } = splitRef.current;
+      gsap.set(chars, { x: 100, opacity: 0, skewX: 20 });
+
+      const charMeta = lines.flatMap((line) => {
+        console.log("line", line);
+
+        const lineChars = chars.filter((c) => line.contains(c));
+        return lineChars.map((char, charIndexLine) => ({
+          char,
+          charIndexLine,
+        }));
+      });
+      console.log("this is charMeta with flatMap", charMeta);
+
+      const animate = (tl) => {
+        charMeta.forEach(({ char, charIndexLine }) => {
+          tl.to(
+            char,
+            {
+              x: 0,
+              opacity: 1,
+              skewX: 0,
+              ease: "power4.out",
+              duration: 1.8,
+            },
+            charIndexLine * 0.08,
+          );
+        });
+        return tl;
+      };
+      const tl = gsap.timeline({ paused: true, duration: duration });
+      animate(tl);
+      if (!scrub) {
+        ScrollTrigger.create({
+          trigger: containerRef.current,
+          start: "top top",
+          markers:true,
+          animation: tl,
+          toggleActions: "play reverse  play reverse",
+       
+          
+        });
+      } else {
+        ScrollTrigger.create({
+          trigger: containerRef.current,
+          start:'top top',
+          end:'+=300px',
+          scrub: 3,
+
+          animation: tl,
+          toggleActions: "play reverse  play reverse",
+        });
+      }
+      return () => splitRef.current?.revert();
+    },
+    { scope: containerRef },
+  );
+  return (
+    <>
+      <section
+        ref={containerRef}
+        className="w-full h-svh flex justify-center items-center bg-pink-800"
+      >
+        <h1 ref={textRef} className="text-7xl w-1/2 text-purple-500 ">
+          {text}
+        </h1>
+      </section>
+    </>
+  );
 }
--- src/app/gsap/textAnimationExample/page.js
+++ src/app/gsap/textAnimationExample/page.js
@@ -1,65 +1,16 @@
 "use client"
 import { ReactLenis } from 'lenis/react'
-import {useGSAP} from '@gsap/react'
-import gsap from 'gsap' 
-import {useRef} from 'react'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import splitText from 'gsap/SplitText';
+
+import AnimatedText from '../components/AnimatedText'
+
 export default function TextAnimationExample() {
-    gsap.registerPlugin(ScrollTrigger,splitText)
-    const firstTextRef=useRef(null)
-    const splitRef=useRef(null)
-    useGSAP(()=>{
-       const el=firstTextRef.current
-       if(!el) return
 
-        splitRef.current=splitText.create('#text-wrapper-one',{
-            type:"lines,words,chars",
-            lineClass:"text-animation-line",
-            wordClass:"text-animation-word",
-            charClass:"text-animation-char",
-            autoSplit:true
-        })
-        console.log('see whats inside me',splitRef.current)
-        const { lines, chars } = splitRef.current
-        gsap.set(chars, { x:100,opacity:0,skewX:20 })
 
-        
-        const charMeta=lines.flatMap((line)=>{
-            console.log('line',line)
-          
-            const lineChars=chars.filter((c)=>line.contains(c))
-            return lineChars.map((char,charIndexLine)=>({
-                char,
-                charIndexLine,
-            }))
-        })
-        console.log("this is charMeta with flatMap",charMeta)
-     
-        const animate=(tl)=>{
-            charMeta.forEach(({char,charIndexLine})=>{
-                tl.to(char,{
-                    x:0,
-                    opacity:1,
-                    skewX:0,
-                    ease:'power4.out',
-                    duration:1.8,
-                },
-                charIndexLine*0.08
-            )
-                
-            })
-            return tl
-        }
-        const tl=gsap.timeline({});
-        animate(tl)
-       return()=>splitRef.current?.revert()        
-    },{scope:firstTextRef})
 
     return (
         <>
             <ReactLenis root>
-                <section ref={firstTextRef} className="h-svh w-full flex justify-center items-center bg-purple-600">
+                {/* <section ref={firstTextRef} className="h-svh w-full flex justify-center items-center bg-purple-600">
                     <h1 id="text-wrapper-one" className="w-[65%] text-center text-[clamp(3rem,8vw,10rem)] tracking-wider ">lorem ipsum is great and i love this kind of animation</h1>
                 </section>
              
@@ -68,7 +19,10 @@ export default function TextAnimationExample() {
                 </section>
                 <section>
 
-                </section>
+                </section> */}
+                <AnimatedText text={'HELLO MY NAME IS COMPONENT'} scrub={true} />
+                 <AnimatedText text={'HELLO MY NAME IS COMPONENT'} />
+                  <AnimatedText text={'HELLO MY NAME IS COMPONENT'} />
 
             </ReactLenis>
         </>
