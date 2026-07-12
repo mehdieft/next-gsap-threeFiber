bec8b92909935fd352b177a05e17bb0e5ea67723
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2026-07-12 21:45:08 +0330

    ok

--- src/app/r3f/theatre/page.js
+++ src/app/r3f/theatre/page.js
@@ -2,23 +2,29 @@
 import { Experience } from "@/app/components/3D/theatre/Experience";
 import { Canvas } from "@react-three/fiber";
 import { getProject } from "@theatre/core";
-import { SheetProvider } from '@theatre/r3f'
+import { PerspectiveCamera, SheetProvider } from '@theatre/r3f'
 import studio from '@theatre/studio'
 import extension from '@theatre/r3f/dist/extension'
-
+import { editable as e } from "@theatre/r3f";
+import { useRef } from "react";
 studio.initialize()
 studio.extend(extension)
 
 const demoSheet = getProject('Demo Project').sheet('Demo Sheet')
 
 export default function Theatre(){
+    const cameraTarget=useRef()
     return(
         <>
         <div className="w-full h-svh bg-green-800">
-            <Canvas>
+            <Canvas gl={{preserveDrawingBuffer:true}}>
                 <SheetProvider sheet={demoSheet}>
-
+                <PerspectiveCamera position={[0,0,10]} theatreKey="camera"  makeDefault fov={30} far={90} near={1} lookAt={cameraTarget.current?.position} />
                 <Experience/>
+                <e.mesh theatreKey="camera target" scale={0.2} visible="editor" position={[0,0,-5]} ref={cameraTarget} >
+                    <octahedronGeometry args={[1,0]} />
+                    <meshPhongMaterial color="pink" />
+                </e.mesh>
                 </SheetProvider>
             </Canvas>
         </div>
