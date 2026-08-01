96bb6c69623d62edd7dcc1d2a4a4b9d481b9bf64
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2026-08-01 18:36:52 +0330

    updating prisma logic

--- src/app/lib/prisma.js
+++ src/app/lib/prisma.js
@@ -2,8 +2,20 @@ import "dotenv/config";
 import { PrismaClient } from "@/generated/prisma";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
+if (!process.env.DATABASE_URL) {
+	process.env.DATABASE_URL = "file:./dev.db";
+}
 
-const connectionString = process.env.DATABASE_URL || "file:./dev.db";
-const adapter = new PrismaBetterSqlite3({ url: connectionString });
-const prisma = new PrismaClient({ adapter });
-export { prisma };
+const globalForPrisma = globalThis;
+const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
+
+export const prisma =
+	globalForPrisma.prisma ||
+	new PrismaClient({
+		adapter,
+		log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+	});
+
+if (process.env.NODE_ENV !== "production") {
+	globalForPrisma.prisma = prisma;
+}
