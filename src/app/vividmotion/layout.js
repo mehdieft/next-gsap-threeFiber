430f85aed44d1f2d70c63dcf2344af43866cbd2a
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2026-08-03 18:49:12 +0330

    adding lennis react and sync it with gsap scrolltrigger

--- src/app/vividmotion/layout.js
+++ src/app/vividmotion/layout.js
@@ -1,4 +1,12 @@
+"use client";
+import ReactLenis from "lenis/react";
 import localFont from "next/font/local";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 
 const samim = localFont({
   src: [
@@ -7,7 +15,7 @@ const samim = localFont({
       weight: "300",
       style: "normal",
     },
- 
+
     {
       path: "../../fonts/vividmotion/Samim-Bold.woff",
       weight: "700",
@@ -19,9 +27,36 @@ const samim = localFont({
 
 
 export default function Layout({ children }) {
+  const lenisRef = useRef();
+
+  useEffect(() => {
+    const lenis = lenisRef.current?.lenis;
+
+    if (!lenis) return;
+
+    lenis.on("scroll", ScrollTrigger.update);
+
+    const update = (time) => {
+      lenis.raf(time * 1000);
+    };
+
+    gsap.ticker.add(update);
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      gsap.ticker.remove(update);
+    };
+  }, []);
   return (
     <div className={samim.className}>
-      {children}
+      <ReactLenis ref={lenisRef}
+        root
+        options={{
+          duration: 1.2,
+          smoothWheel: true,
+        }}>
+        {children}
+      </ReactLenis>
     </div>
   );
 }
