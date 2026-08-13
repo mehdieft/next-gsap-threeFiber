1c45759ae62f4f92796ea31785192fcf9f301cc3
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2026-08-13 20:44:53 +0330

    i dont want exand for now

--- src/app/vividmotion/services/page.js
+++ src/app/vividmotion/services/page.js
@@ -1,17 +1,11 @@
 import { ServicesText } from "@/app/components/vividmotion/servicesText";
-import ExpandText from "@/app/components/vividmotion/expandText";
 export default function ServicesPage() {
     return (
         <>
-            <div className="w-screen h-[50vh] bg-black relative flex flex-col gap-10 py-20 justify-center items-center">
+            <div className="w-screen h-[70vh] bg-black relative flex flex-col gap-10 py-20 justify-center items-center">
                 <ServicesText />
-                <div className="relative h-20 w-full">
-
-                    <div className="absolute bottom-0 right-1/2 translate-x-1/2">
-
-                        <ExpandText />
-                    </div>
-                </div>
+                {/* <div className="relative h-20 w-full">
+                </div> */}
             </div>
         </>
     );
