ab26cd183be624df8afc692236ed982c1226e605
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2026-08-19 18:44:36 +0330

    try to work with gsap

---
+++ src/app/r3f/silencio/page.js
@@ -0,0 +1,16 @@
+export default function Selencio(){
+    return(
+        <>
+        <div className="model"></div>
+        <section className="hero" >
+            <h1>Digital <br/> Evolution</h1>
+            <h2>Transform your brand identity</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </section>
+        <section className="info" ></section>
+        <section className="scanner" ></section>
+        <section className="outro" ></section>
+        </>
+    )
+
+}
